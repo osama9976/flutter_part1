1f34e797d6df701c09557674e6ff131705de8235
osama aloriny <omo.7676@hotmail.com>
2019-02-27 20:13:46 +0300

    add vo

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),                             // ... to here.
       home: new RandomWords(),
     );
 //    final wordPair = new WordPair.random(); // Add this line.
@@ -40,7 +43,40 @@ class RandomWords extends StatefulWidget{
 
 class  RandomWordsState extends State<RandomWords>
 {
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return new Scaffold(         // Add 6 lines from here...
+            appBar: new AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );                           // ... to here.
+        },
+      ),
+    );
+  }
+
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();   // Add this line.
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   @override                                  // Add from this line ...
   Widget build(BuildContext context) {
@@ -49,10 +85,16 @@ class  RandomWordsState extends State<RandomWords>
     return new Scaffold (                   // Add from here...
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[      // Add 3 lines from here...
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],                      // ... to here.
+
       ),
       body: _buildSuggestions(),
-    );                                      // ... to here.
-  }
+    );
+
+
+ }
 
 
 
@@ -76,11 +118,27 @@ class  RandomWordsState extends State<RandomWords>
 
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);  // Add this line.
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+
+      trailing: new Icon(   // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),                    // ... to here.
+
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },               // ... to here.
     );
   }
 
